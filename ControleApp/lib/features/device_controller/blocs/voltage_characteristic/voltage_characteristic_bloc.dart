fa5d1db6c01a5bed89c81256dc4b55affc34100d
Fritz Körbel <fritz.koerbel96@gmail.com>
2024-04-10 00:40:41 +0200

    implemented Voltage characteristic

--- ControleApp/lib/features/device_controller/blocs/voltage_characteristic/voltage_characteristic_bloc.dart
+++ ControleApp/lib/features/device_controller/blocs/voltage_characteristic/voltage_characteristic_bloc.dart
@@ -1,48 +1,23 @@
 import 'dart:typed_data';
 
-import 'package:controleapp/features/device_controller/blocs/time_characteristic/time_characteristic_event.dart';
-import 'package:controleapp/features/device_controller/blocs/time_characteristic/time_characteristic_state.dart';
+import 'package:controleapp/features/device_controller/blocs/voltage_characteristic/voltage_characteristic_event.dart';
+import 'package:controleapp/features/device_controller/blocs/voltage_characteristic/voltage_characteristic_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class VoltageCharacteristicBloc
-    extends Bloc<TimeCharacteristicEvent, TimeCharacteristicState> {
+    extends Bloc<VoltageCharacteristicEvent, VoltageCharacteristicState> {
   VoltageCharacteristicBloc(super.initialState, this.characteristic) {
-    on<Write>(_write);
     on<Read>(_read);
   }
-
   final BluetoothCharacteristic characteristic;
 
-  Future<void> _write(
-    Write event,
-    Emitter<TimeCharacteristicState> emit,
-  ) async {
-
-    final now = DateTime.now();
-    //this is a bit hacky but i dont see a better way
-    final nowUtc = DateTime.utc(
-        now.year, now.month, now.day, now.hour, now.minute, now.second);
-
-    final secondsSinceEpoch = nowUtc.millisecondsSinceEpoch ~/ 1000;
-    final payload = Uint8List(8);
-    ByteData.view(payload.buffer).setUint64(0, secondsSinceEpoch);
-
-    await characteristic.write(payload);
-    add(const Read());
-  }
-
-  Future<void> _read(Read event, Emitter<TimeCharacteristicState> emit) async {
+  Future<void> _read(Read event, Emitter<VoltageCharacteristicState> emit) async {
     final result = await characteristic.read();
     final payload = Uint8List.fromList(result);
 
-    final secondsSinceEpoch = ByteData.view(payload.buffer).getUint64(0);
+    final voltage = ByteData.view(payload.buffer).getFloat32(0);
 
-    final newTime = DateTime.fromMillisecondsSinceEpoch(
-      secondsSinceEpoch * 1000,
-      isUtc: true,
-    );
-
-    emit(state.copyWith(time: newTime));
+    emit(state.copyWith(voltage: voltage));
   }
 }
--- ControleApp/lib/features/device_controller/widgets/voltage_characteristic.dart
+++ ControleApp/lib/features/device_controller/widgets/voltage_characteristic.dart
@@ -1,12 +1,44 @@
+import 'package:controleapp/features/device_controller/blocs/voltage_characteristic/voltage_characteristic_bloc.dart';
+import 'package:controleapp/features/device_controller/blocs/voltage_characteristic/voltage_characteristic_event.dart';
+import 'package:controleapp/features/device_controller/blocs/voltage_characteristic/voltage_characteristic_state.dart';
 import 'package:controleapp/features/device_controller/widgets/characteristic.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class VoltageCharacteristic extends AirsoftCharacteristic{
+class VoltageCharacteristic extends AirsoftCharacteristic {
   const VoltageCharacteristic({required super.characteristic, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('VoltageCharacteristic');
-  }
+    return BlocProvider(
+      create: (_) => VoltageCharacteristicBloc(
+        const VoltageCharacteristicState(voltage: 0),
+        characteristic,
+      ),
+      child: BlocBuilder<VoltageCharacteristicBloc, VoltageCharacteristicState>(
+        builder: (BuildContext context, VoltageCharacteristicState state) {
+          final voltageCharacteristicBloc =
+              context.read<VoltageCharacteristicBloc>();
 
+          return Row(
+            children: [
+              const Text('voltage'),
+              Expanded(child: Container()),
+              Text(state.voltage.toString()),
+              Expanded(child: Container()),
+              Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () =>
+                        voltageCharacteristicBloc.add(const Read()),
+                    child: const Text('read'),
+                  ),
+                ],
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
 }
